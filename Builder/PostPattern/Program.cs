a7b3dd2332fed9dac965c3c1aae91c1a1ac5efc9
Gerson Azabache <gerson_dunk@hotmail.com>
2023-11-24 13:20:10 -0500

    builder pattern pre and post completed

--- Builder/PostPattern/Program.cs
+++ Builder/PostPattern/Program.cs
@@ -4,12 +4,13 @@ using PostPattern.Entities;
 
 Console.WriteLine("Builder Pattern Applied!");
 
+//Creo un objeto empleado y me olvido de crear más objetos, simplemente seteo la información
 Empleado empleado = new Empleado.EmpleadoBuilder()
     .SetId(123)
     .SetNombre("Gerson")
     .SetFechaIngreso(new DateTime(2020,12,20))
-    .SetArea("Sistemas")
-    .SetCargo("Asistente")
+    .SetArea("IT")
+    .SetCargo(".NET Engineer")
     .SetDireccion("Calle Begonias 23","Lima","Peru","54321")
     .AddTelefonos(51,789545521,"Móvil")
     .AddTelefonos(1,985552000,"Whatsapp")
--- Builder/PrePattern/Program.cs
+++ Builder/PrePattern/Program.cs
@@ -1,16 +1,28 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using PrePattern.Entities;
+
 Console.WriteLine("Example without Builder pattern!");
 
-Empleado empleado = new Empleado.EmpleadoBuilder()
-    .SetId(123)
-    .SetNombre("Gerson")
-    .SetFechaIngreso(new DateTime(2020,12,20))
-    .SetArea("Sistemas")
-    .SetCargo("Asistente")
-    .SetDireccion("Calle Begonias 23","Lima","Peru","54321")
-    .AddTelefonos(51,789545521,"Móvil")
-    .AddTelefonos(1,985552000,"Whatsapp")
-    .AddTelefonos(51,8526320,"Fijo")
-    .Build();
+//Creo el objeto empleado
+Empleado empleado = new Empleado();
+empleado.IdEmpleado = 123;
+empleado.Nombre = "Gerson";
+empleado.FechaIngreso = new DateTime(2020, 12, 20);
+
+//Creo los objetos compuestos de empleado
+Area area = new Area("IT");
+Cargo cargo = new Cargo(".NET Engineer");
+Direccion direccion = new Direccion("Calle Begonias 23","Lima","Peru","54321");
+List<Telefono> telefonos = new List<Telefono>(){
+    new Telefono(51, 789545521, "Móvil"),
+    new Telefono(1, 985552000, "Whatsapp"),
+    new Telefono(51, 8526320, "Fijo")
+};
+//Asigno los objetos compuestos al objeto principal empleado
+empleado.Area = area;
+empleado.Cargo = cargo;
+empleado.Direccion = direccion;
+empleado.Telefonos = telefonos;
+
 
 Console.WriteLine(empleado);
